34ab083242b9169128ec6b937fd3f9742f531481
George Joseph <thegeorgejoseph@gmail.com>
2021-04-30 20:20:19 +0530

    Setting up a Parameters dictionary

--- cowin-tracker.py
+++ cowin-tracker.py
@@ -1,9 +1,14 @@
-'''
-Creating a simple script to track changes in the Cowin Portal and send you an email when there is a slot available
-
-'''
-
+#Importing the required libraries
 import requests
 
+#Setting up the parameters that need to be passed
+parameters = {
+    "header":"hi_IN",
+    "pincode":"412307",
+    "date":"01-05-2021"
+}
 #Hitting the COWIN Public API  
-res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?header=hi_IN&pincode=412307&date=01-05-2021")
+res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin",params=parameters)
+
+print(res.status_code)
+
