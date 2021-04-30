6738efa95ac9adcbcf18e229166852b108283bcb
George Joseph <thegeorgejoseph@gmail.com>
2021-04-30 21:54:01 +0530

    Retrieving Information from the API

--- cowin-tracker.py
+++ cowin-tracker.py
@@ -8,15 +8,6 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-#Setting up the parameters that need to be passed
-parameters = {
-    "header":"hi_IN",
-    # "pincode":"412307",
-    "date":"01-05-2021",
-    "district_id":"17", #683
-
-}
-
 #Code that can be used to generate the State ID's
 # for i in range(1,40):
 #     res  = requests.get(f"https://cdn-api.co-vin.in/api/v2/admin/location/districts/{i}")
@@ -27,9 +18,29 @@ parameters = {
 
 # For our purposes. state_id = 17 is what we want to generate the 
 
+#Setting up the parameters that need to be passed
+parameters = {
+    "header":"hi_IN",
+    "date":"01-05-2021",
+    "district_id":"270",
+
+}
+
+
 #Hitting the COWIN Public API  
 res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict",params=parameters)
 
-print(res.status_code)
+if res.status_code == 200:
+    data = res.json()
+    for obj in data['centers']:
+        print(obj["name"])
+        print(obj["district_name"])
+        print(obj["block_name"])
+        print(obj["pincode"])
+        for i in range(len(obj["sessions"])):
+            print(obj["sessions"][i]["date"])
+            print(obj["sessions"][i]["available_capacity"])
+            print(obj["sessions"][i]["vaccine"])
+        print("******")
 
-jprint(res.json())
+        
