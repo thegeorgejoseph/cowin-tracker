d582cd327e883dc5e39ea7e3151cabed821de534
George Joseph <thegeorgejoseph@gmail.com>
2021-04-30 20:17:09 +0530

    Hitting the API

--- cowin-tracker.py
+++ cowin-tracker.py
@@ -3,3 +3,7 @@ Creating a simple script to track changes in the Cowin Portal and send you an em
 
 '''
 
+import requests
+
+#Hitting the COWIN Public API  
+res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?header=hi_IN&pincode=412307&date=01-05-2021")
