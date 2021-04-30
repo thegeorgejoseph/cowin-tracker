d0e3f9d8b0f210e56543610d583d8d14268d70bc
George Joseph <thegeorgejoseph@gmail.com>
2021-04-30 21:27:07 +0530

    Hitting the state api to get the list of districts

--- cowin-tracker.py
+++ cowin-tracker.py
@@ -1,14 +1,35 @@
 #Importing the required libraries
 import requests
+import json
+
+#Function that converts json object in Human Readable Form
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 #Setting up the parameters that need to be passed
 parameters = {
     "header":"hi_IN",
-    "pincode":"412307",
-    "date":"01-05-2021"
+    # "pincode":"412307",
+    "date":"01-05-2021",
+    "district_id":"17", #683
+
 }
+
+#Code that can be used to generate the State ID's
+# for i in range(1,40):
+#     res  = requests.get(f"https://cdn-api.co-vin.in/api/v2/admin/location/districts/{i}")
+
+#     print(res.status_code)
+
+#     jprint(res.json())
+
+# For our purposes. state_id = 17 is what we want to generate the 
+
 #Hitting the COWIN Public API  
-res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin",params=parameters)
+res  = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict",params=parameters)
 
 print(res.status_code)
 
+jprint(res.json())
